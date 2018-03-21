15ead94bda099bc6e455b2779a6056461de8b6a2
Patrik Kemeny <patrikkemeny@Patriks-MacBook-Pro.local>
2018-03-21 13:43:10 +1100

    lesson-03

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ChannelVC: UIViewController {
-
+    //Outlets
+    
+    @IBOutlet weak var loginBtn: UIButton! // shoudl change when log in to user name
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         //the measure how much the rear view controller is show
     }
 
-
+    @IBAction func loginBtnPresse(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
 }
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Patrik Kemeny on 21/3/18.
+//  Copyright © 2018 Patrik Kemeny. All rights reserved.
+//
+
+//use this file for Seque identifiers
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
